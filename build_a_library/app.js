ce6c262117acd799837da8f790b918f1c9f8c0e6
Chris_felicie <f.ilungakalenga@gmail.com>
2024-05-24 12:59:07 +0200

    Test the movie instance

--- build_a_library/app.js
+++ build_a_library/app.js
@@ -1,8 +1,8 @@
 class Media {
-  constructor(title, isCheckedOut, ratings) {
+  constructor(title) {
     this._title = title;
-    this._isCheckedOut = isCheckedOut;
-    this._ratings = ratings;
+    this._ratings = [];
+    this._isCheckedOut = false;
   }
   get title() {
     return this._title;
@@ -20,7 +20,7 @@ class Media {
   }
 
   toggleCheckOutStatus() {
-    return !this._isCheckedOut;
+    return (this._isCheckedOut = !this._isCheckedOut);
   }
 
   addRating(val) {
@@ -28,12 +28,54 @@ class Media {
   }
 
   getAverageRating() {
-    return this._ratings.reduce(
+    const sumRating = this._ratings.reduce(
       (currentSum, rating) => (currentSum += rating),
       0
     );
+
+    return sumRating / this._ratings.length;
   }
 }
-class Book {}
+class Book extends Media {
+  constructor(author, title, pages) {
+    super(title);
+    this._author = author;
+    this._pages = pages;
+  }
+
+  get author() {
+    return this._author;
+  }
+  get pages() {
+    return this._pages;
+  }
+
+  //
+}
+
+//
+//
+
+class Movies extends Media {
+  constructor(title, director, runTime) {
+    super(title);
+    this._director = director;
+    this._runTime = runTime;
+  }
+}
 class CD {}
-class Movies {}
+
+// create Book
+
+const historyOfEverything = new Book(
+  "Bill Bryson",
+  "A Short History of Nearly Everything",
+  544
+);
+
+historyOfEverything.toggleCheckOutStatus();
+historyOfEverything.addRating(4);
+historyOfEverything.addRating(5);
+historyOfEverything.addRating(5);
+
+console.log(historyOfEverything.getAverageRating());
